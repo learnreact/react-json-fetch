fa370028dfb95cdf1e3fc63f15fdd0fe454f01b6
Michael Chan <mijoch@gmail.com>
2017-08-09 16:11:51 -0700

    fix: don't import Component directly
    This fixes some downstream issues I'm having using Rollup and this lib.
    
    reference: https://github.com/reactjs/react-redux/issues/643

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react"
+import React from "react"
 
-class Fetch extends Component {
+class Fetch extends React.Component {
   constructor() {
     super()
     this.state = {}
